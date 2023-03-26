9848d7a4ea61d78d9ee3bebddff742d2fd9da72e
Asif Raza <90905808+asifraza03@users.noreply.github.com>
2023-03-26 11:52:53 +0530

    Add files via upload
    Here,In JS DOM(1) I learn how to create,delte,Update using JavaScript DOM Method.

---
+++ JS DOM(1)/script.js
@@ -0,0 +1,9 @@
+//thats how we delete the  para tag --------
+
+function myFunction() {
+  const element = (document.getElementById("demo").innerHTML = " ");
+}
+
+//--------Update the h3 tag here-------
+
+document.getElementById("myh3").innerHTML = "New text!";
